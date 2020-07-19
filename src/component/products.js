5be22cf600f2f00d3bb04bafc989e53b83258a89
Kwangjin Kim <ggj101@naver.com>
2020-07-20 01:59:32 +0900

    상품 페이지 기능구현
    옵션/추가 상품 기능 구현

--- src/component/products.js
+++ src/component/products.js
@@ -13,11 +13,12 @@ function Products(){
     const [prdOption,setPrdOption] = useState([])
     const [addPrd,setAddPrd] = useState([])
     const [deliverySelValue,setDeliverySelValue] = useState("1")
-    const [optionSelValue,setOptionSelValue] = useState()
+    const [optionListArr,setOptionListArr] = useState([]);
+    const [addPrdListArr,setAddPrdListArr] = useState([]);
+    const [totalCount,setTotalCount] = useState(0);
+    const [totalPrice,setTotalPrice] = useState(0);
     
-    const {category_list,product_code,product_name,product_price} = prdData;
-
-    
+    const {product_code,product_name,product_price} = prdData;
 
     useEffect(()=>{
         Axios.get(process.env.PUBLIC_URL+'/ProductsData.json').then((response)=>{
@@ -29,12 +30,182 @@ function Products(){
             setDeliveryMethod(response.data.delivery_method);
             setPrdOption(response.data.option);
             setAddPrd(response.data.add_product);
-            console.log('테스트');
         })
     },[])
 
+    const onOptionValue = (data,e) =>{
+        const ReselectionFilter = optionListArr.filter((arr)=>{
+            return arr[0].data_id === data;
+        })
+        
+        const ReselectionFilter2 = ReselectionFilter.filter((arr)=>{
+            return arr[0].id === parseInt(e.target.value);
+        })
+
+        if(ReselectionFilter2.length)
+        {
+            return alert('이미 선택한 옵션입니다.');
+        }
+
+        if(e.target.value !== '0')
+        {
+            const copy = [...prdOption]
+            const prdFilter = copy.filter((arr)=>{
+                return  arr.id === data
+            })
+         
+            const list = prdFilter[0].option_list;
+    
+            const prdFilter2 = list.filter((arr)=>{
+                return arr.id === parseInt(e.target.value);
+            })
+
+            if(prdFilter2[0].soldout)
+            {
+                e.target.value = '0';
+                return alert('품절인 옵션은 구매하실 수 없습니다.');
+            }
+            else{
+                if(prdFilter2[0].add_price)
+                {
+                    setTotalPrice(totalPrice+product_price+prdFilter2[0].add_price);
+                }
+                else setTotalPrice(totalPrice+product_price);
+                const listMap = prdFilter2.map((arr)=>{
+                    return {data_id:data,count:1,...arr};
+                })
+                
+                console.log(optionListArr);
+                setOptionListArr([...optionListArr,listMap])
+            }
+        }
+    }
+
+    
+    const onAddPrdValue = (data,e) =>{
+        const ReselectionFilter = addPrdListArr.filter((arr)=>{
+            return arr[0].data_id === data;
+        })
+        
+        const ReselectionFilter2 = ReselectionFilter.filter((arr)=>{
+            return arr[0].id === parseInt(e.target.value);
+        })
+
+        if(ReselectionFilter2.length)
+        {
+            return alert('이미 선택한 옵션입니다.');
+        }
+
+        if(e.target.value !== '0')
+        {
+            const copy = [...addPrd]
+            const prdFilter = copy.filter((arr)=>{
+                return arr.id === data
+            })
+         
+            const list = prdFilter[0].product_list;
+            const prdFilter2 = list.filter((arr)=>{
+                return arr.id === parseInt(e.target.value);
+            })
+
+            if(prdFilter2[0].soldout)
+            {
+                e.target.value = '0';
+                return alert('품절인 옵션은 구매하실 수 없습니다.');
+            }
+            else{
+                setTotalPrice(totalPrice+prdFilter2[0].price);
+                const listMap = prdFilter2.map((arr)=>{
+                    return {data_id:data,count:1,...arr};
+                })
+
+                setAddPrdListArr([...addPrdListArr,listMap])
+            }
+        }
+    }
+
+    const optionList = optionListArr.map((arr,num)=>{
+        return(
+            <li>
+                {console.log()}
+                <span className="list_prd">{arr[0].name}</span>
+                <div className="list_count">
+                    <button className="delBtn" onClick={()=>onOptionRemove(num)}>X</button>
+                    <span className="list_price">{arr[0].add_price ? arr[0].count*(arr[0].add_price + product_price) : arr[0].count*(product_price)}</span>
+                    <input className="list_input" value={arr[0].count} onChange={(e)=>onOptionCount(e,num)}/>
+                </div>
+            </li>
+        )
+    })
+
+    const onOptionCount = (e,num) =>{
+         const countMap = optionListArr.map((arr,arrIndex)=>{
+            if(arrIndex === num)
+            {
+                arr[0].count = parseInt(e.target.value);
+            }
+            return arr;
+         })
+
+         setOptionListArr(countMap);
+    }
+
+    const addPrdList = addPrdListArr.map((arr,num)=>{
+        return(
+            <li>
+                {console.log()}
+                <span className="list_prd">{arr[0].name}</span>
+                <div className="list_count">
+                    <button className="delBtn" onClick={()=>onAddPrdRemove(num)}>X</button>
+                    <span className="list_price">{arr[0].price}</span>
+                    <input className="list_input" value={arr[0].count} onChange={(e)=>onAddPrdCount(e,num)}/>
+                </div>
+            </li>
+        )
+    })
+
+    const onAddPrdCount = (e,num) =>{
+        const countMap = addPrdListArr.map((arr,arrIndex)=>{
+           if(arrIndex === num)
+           {
+               arr[0].count = parseInt(e.target.value);
+           }
+           return arr;
+        })
+
+        setAddPrdListArr(countMap);
+   }
+
+    const onOptionRemove = (num) =>{
+        const priceFilter = optionListArr.filter((arr,fil_num)=>{
+            return fil_num === num;
+        })
+        if(priceFilter[0][0].add_price)
+        {
+            setTotalPrice(totalPrice-product_price-priceFilter[0][0].add_price)
+        }
+        else setTotalPrice(totalPrice-product_price);
+
+        const removeFilter = optionListArr.filter((arr,fil_num)=>{
+            return fil_num !== num;
+        })
+     
+        setOptionListArr(removeFilter);
+     }
+
+     const onAddPrdRemove = (num) =>{
+        const priceFilter = addPrdListArr.filter((arr,fil_num)=>{
+            return fil_num === num;
+        })
+
+        setTotalPrice(totalPrice-priceFilter[0][0].price);
+        const removeFilter = addPrdListArr.filter((arr,fil_num)=>{
+            return fil_num !== num;
+        })
+        setAddPrdListArr(removeFilter);
+     }
+
     const onDeliverySel=(e)=>{
-        console.log(e);
         setDeliverySelValue(e.target.options[e.target.selectedIndex].value);
     }
 
@@ -98,7 +269,6 @@ function Products(){
                                         <dd>
                                             <div className="area_cost">
                                                 <strong className="info_cost">
-                                                    {console.log(saleInfo)}
                                                     <span className="price">{product_price}<span>원</span></span>
                                                     {saleInfo.length !== 0 ?
                                                         <> 
@@ -141,11 +311,11 @@ function Products(){
                                             {
                                                 prdOption.map((option_item) => {
                                                     return (
-                                                        <select>
-                                                            <option value={option_item.id}>{option_item.name}</option>
+                                                        <select onChange={(e) => onOptionValue(option_item.id, e)}>
+                                                            <option value='0'>{option_item.name}</option>
                                                             {
                                                                 option_item.option_list.map((list)=>{
-                                                                    return <option value={list.id}>{list.name}</option>
+                                                                    return <option value={list.id}>{list.name}{list.add_price ? "  ("+list.add_price+"원)추가" : ''} {list.soldout ? "(품절)": ''}</option>
                                                                 })
                                                             }
                                                         </select>
@@ -160,11 +330,11 @@ function Products(){
                                                 {
                                                     addPrd.map((addItem) => {
                                                         return (
-                                                            <select>
-                                                                <option value={addItem.id}>{addItem.name}</option>
+                                                            <select onChange={(e) => onAddPrdValue(addItem.id, e)}>
+                                                                <option value='0'>{addItem.name}</option>
                                                                 {
                                                                     addItem.product_list.map((list)=>{
-                                                                        return <option value={list.id}>{list.name} {list.price}원+</option>
+                                                                     return <option value={list.id}>{list.name} {list.price}원 {list.soldout ? "(품절)": ''}</option>
                                                                     })
                                                                 }
                                                             </select>
@@ -175,11 +345,16 @@ function Products(){
                                         </div>
                                     </div>
 
+                                    <ul className="purchase_list">
+                                        {optionList}
+                                        {addPrdList}
+                                    </ul>
+
                                     <div className="total">
-                                        <span className="total_count">총 수량 0개</span>
+                                            <span className="total_count">총 수량 {totalCount}개</span>
                                         <span className="total_cost">
                                             총 상품금액
-                                            <span>0원</span>
+                                            <span>{totalPrice}원</span>
                                         </span>
                                     </div>
 
