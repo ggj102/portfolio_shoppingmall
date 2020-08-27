8c49c0f544cc4392b59b1104f6b8734ee46890a0
Kwangjin Kim <ggj101@naver.com>
2020-08-27 22:34:38 +0900

    메인 페이지 마무리
    메인페이지 상단 카테고리 마무리

--- src/component/mainpage/MainPageHeader.js
+++ src/component/mainpage/MainPageHeader.js
@@ -6,44 +6,107 @@ function MainPageHeader()
 {
     const [headerData,setHeaderData] = useState({});
     const [categoryData,setCategoryData] = useState([]);
-    const [mouseOverState,setMouseOverState] = useState(0);
+    const [subCategoryData,setSubCategoryData] = useState([]);
+    const [subData1,setSubData1] = useState([]);
+    const [subData2,setSubData2] = useState([]);
+    const [subData3,setSubData3] = useState([]);
+    const [depthNum,setdepthNum] = useState({depth1:0, depth2:false, depth3:false})
 
     useEffect(()=>{
         Axios.get(process.env.PUBLIC_URL+'/MainPageData/MainPageHeader.json').then((response)=>{
             console.log(response.data);
             setHeaderData(response.data);
             setCategoryData(response.data.category_list);
+            setSubCategoryData(response.data.subcategory_list)
         })
     },[])
 
-    const categoryMouseOver = (id) =>{
-        setMouseOverState(id);
+    const categoryMouseOver = (id,num) =>{
+        const subfilter =  subCategoryData.filter((arr)=>{
+            return arr.parent_id === id;
+        })
+
+        if(subfilter.length === 0)
+        {
+            if(num === 2)
+            {
+               setSubData2([]);
+               setSubData3([]);
+               return setdepthNum({...depthNum,depth2:false,depth3:false});
+            }
+            else if(num === 3)
+            {
+                setSubData3([]);
+                return setdepthNum({...depthNum,depth3:false});
+            }
+        }
+        else{
+            if(num === 1)
+            {
+                setdepthNum({...depthNum,depth1:id,depth2:false,depth3:false});
+                setSubData1(subfilter);
+            }
+            else if(num === 2)
+            {
+                setdepthNum({...depthNum,depth2:true});
+                setSubData2(subfilter);
+            }
+            else if(num === 3)
+            {
+                setdepthNum({...depthNum,depth3:true});
+                setSubData3(subfilter);
+            }
+        }
     }
 
     const categoryMouseOut = () =>{
-        setMouseOverState(0);
+        setdepthNum({...depthNum,depth1:0,depth2:false,depth3:false});
+        setSubData1([]);
+        setSubData2([]);
+        setSubData3([]);
+    }
+
+    const subDataMap = (data,num) =>{
+        return(
+            <div className="categoryMouseOver_list">
+                <ul>
+                    {data.map((sub)=>{
+                        return(
+                            <li className="category_name2" 
+                                onMouseEnter={()=>categoryMouseOver(sub.id,num)}
+                            >{sub.name}</li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
     }
 
     const categoryMap = categoryData.map((arr)=>{
         return(
-            <li>
-                <div className="category_name" 
-                onMouseOver={()=>categoryMouseOver(arr.id)}
-                onMouseOut={categoryMouseOut}
+            <li className="category_inner_li" onMouseLeave={categoryMouseOut}>
+                <div className="category_name" onMouseEnter={()=>categoryMouseOver(arr.id,1)}
                 >{arr.name}</div>
-                {arr.id === mouseOverState ? 
-                    <div className="categoryMouseOver_list">
 
-                </div>: ''}
-                
-                {/* <select>
-                    <option>{arr.name}</option>
-                    {arr.sub_category.map((arr2)=>{
-                        return(
-                            <option>{arr2.name}</option>
-                        )
-                    })}
-                </select> */}
+            {depthNum.depth1 === arr.id &&
+                <div className="mouseOver_area" >
+                    <div className="category_depth1">
+
+                        {subDataMap(subData1,2)}
+
+                        {depthNum.depth2 && 
+                            <div className="category_depth2">
+                              
+                                {subDataMap(subData2,3)}
+                                
+                            {depthNum.depth3 && 
+                                <div className="category_depth3">
+                                  
+                                    {subDataMap(subData3,4)}
+                                </div>}
+                        </div>}
+                    </div>
+                </div>}
             </li>
         )
     })
@@ -93,10 +156,10 @@ function MainPageHeader()
 
             <div className="category_area">
                 <div className="category_inner">
-                    <ul>
+                    <ul className="category_inner_ul">
                         {categoryMap}
                         
-                        <li>
+                        <li className="category_inner_li">
                             <div className="category_btn">
                                 <a href="#">전체상품</a>
                             </div>
